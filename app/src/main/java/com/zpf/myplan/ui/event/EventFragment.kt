04af579a58ad9ab55c5d03ac9902f7b663ee908e
LittleFlyers <2538285284@qq.com>
2022-06-13 23:04:06 +0800

    create nav graph

---
+++ app/src/main/java/com/zpf/myplan/ui/event/EventFragment.kt
@@ -0,0 +1,44 @@
+package com.zpf.myplan.ui.event
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.zpf.myplan.databinding.FragmentDashboardBinding
+
+class EventFragment : Fragment() {
+
+    private lateinit var eventViewModel: EventViewModel
+    private var _binding: FragmentDashboardBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        eventViewModel =
+            ViewModelProvider(this).get(EventViewModel::class.java)
+
+        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textDashboard
+        eventViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
