ff6ef0c3046f4fe388db02bec2d53e85443dbd7a
LittleFlyers <2538285284@qq.com>
2022-06-13 19:48:54 +0800

    create project

---
+++ app/src/main/java/com/zpf/myplan/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.zpf.myplan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
